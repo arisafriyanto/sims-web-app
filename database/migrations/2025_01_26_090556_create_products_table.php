fc579c22777b8504dfd2ed45fae265cd3703b2f8
arisafriyanto <arisafriyanto1933@gmail.com>
2025-01-26 16:41:49 +0700

    feat: setup database

--- database/migrations/2025_01_26_090556_create_products_table.php
+++ database/migrations/2025_01_26_090556_create_products_table.php
@@ -11,15 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id')->constrained()->onDelete('restrict');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('position');
+            $table->decimal('purchase_price', 15, 2);
+            $table->decimal('sale_price', 15, 2);
+            $table->integer('stock');
             $table->string('image')->nullable();
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -29,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
